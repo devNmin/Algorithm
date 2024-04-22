2d7a03922fe7bd13e542fd3549d0dd3922093190
devNmin <104844702+devNmin@users.noreply.github.com>
2024-04-22 13:29:43 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,11 +1,13 @@
+
+# 마이너스로 스플릿함 55-50+40 => ['55','50+40']
 tlst = input().split("-")
 
 ans = 0
-if tlst[0]:
-    tt = list(map(int, tlst[0].split("+")))
+if tlst[0]:     # 마이너스로 스플릿시에 맨앞 값이 존재하면 
+    tt = list(map(int, tlst[0].split("+"))) # +로 스플릿해서 최종값에 합산
     ans += sum(tt)
 
-for t in tlst[1:]:
+for t in tlst[1:]: # 첫번째 이후 부터 똑같이 작업( -로 먼저 스플릿한거기때문에 뒤에 모든값이 -로 합산해줘야 최저값가능)
     tt = list(map(int, t.split("+")))
-    ans -= sum(tt)
+    ans -= sum(tt)      #따라서 뒷부분 모두 값 빼줌
 print(ans)
